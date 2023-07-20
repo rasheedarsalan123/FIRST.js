2a225c3965300242c6dd920d904ef082f8f7b4ac
Arsalan Rasheed <arsalanrasheed198@gmail.com>
2023-07-20 01:45:46 -0700

    created a js file and first practice book chapter 1.2.3.4

---
+++ javascript.js
@@ -0,0 +1,13 @@
+alert("first practice java script")
+
+var firstname="Today Pakistan WIN Tha Match <br>";
+document.write(firstname);
+
+var a =100;
+var b =10;
+
+document.write(a*b);
+
+var a =100;
+var b =10;
+document.write(a+b);
