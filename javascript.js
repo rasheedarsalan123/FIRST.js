43f7ed0d89acf4f1f91b10d67c7162e53154ce1c
Arsalan Rasheed <arsalanrasheed198@gmail.com>
2023-07-26 04:17:59 -0700

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,10 +1,28 @@
-let userFavPlayer=prompt("What is Tha Faveriout player")
-console.log("Babar Azam WORLD Number on Bater "+userFavPlayer)
+//let userFav=prompt("babar azam batter in pakistan");
+//console.log ("babar azam by favourite"+userFav);
 
-let randomNumber=Math.random();
-randomNumber=randomNumber* 40;
-let roundedNumber=Math.round(randomNumber);
-console.log(roundedNumber)
-let a =300+200;
+//et data1=("50")
+//let data2= 50
+//console.log (+data1+data2)
+//let randomnumber=Math.random();
+//randomnumber=randomnumber*20;
+//let roundednumber=Math.round(randomnumber);
+//console.log(roundednumber)
 
-console.log (a)
+//let userAge=+prompt("Enter Yor Age");
+//if ( userAge>=18) {
+  //  console.log ( "YoU Can Applay Cnic");
+//}
+//else console.log("you cant apply cnic");
+//let userNum =+prompt("inter you number");
+//let computerNum = Math.round(Math.random()*10);
+//if (userNum===computerNum){
+  //  console.log("you win")
+//}
+//else console.log("you lose"+computerNum);
+let userAge=+prompt("inter you Age");
+if (userAge<=10){console.log("you allow"); }
+else console.log("you dont allow");
+
+let Table=18
+for (let i=1;i<=10;i++){console.log(Table+"x"+i+" = "+ Table*i);}
